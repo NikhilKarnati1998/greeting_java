b61db3eca6d00ed0903860533ff9aa78d65237a8
NikhilKarnati1998 <nikhilkarnati414@gmail.com>
2021-02-11 11:38:05 +0530

    Freinds cast added

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("F.R.I.E.N.D.S  show");
+    System.out.println("This is chandler");
+    System.out.println("This is joey");
+    System.out.println("This is monica");
+    System.out.println("this is phoebe");
+    System.out.println("This is ROSS");
+    System.out.println("This is RACHEL");
+  }
+}
